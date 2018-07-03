51724013c616b5521eaa872c856010bfb3e6fef5
Trey Shugart <treshugart@gmail.com>
2018-07-03 11:03:51 +1000

    Update bin script to still return path strings.

--- src/bin/index.js
+++ src/bin/index.js
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
 
-const sourceTrace = require('..');
-const yargs = require('yargs');
+const sourceTrace = require("..");
+const yargs = require("yargs");
 const { _, ...opts } = yargs.argv;
 
-console.log(sourceTrace(_[0], opts).join('\n'));
+console.log(
+  sourceTrace(_[0], opts)
+    .map(d => d.path)
+    .join("\n")
+);
