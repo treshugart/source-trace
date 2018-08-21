50e778e3403f6efc02882b553cb551798198245c
Trey Shugart <treshugart@gmail.com>
2018-08-21 12:29:02 +1000

    Update bin.

--- src/bin/index.js
+++ src/bin/index.js
@@ -1,8 +1,10 @@
+#!/usr/bin/env node
+
 const { trace } = require("..");
 const yargs = require("yargs");
 const { _, ...opts } = yargs.argv;
 
 (async () => {
   const traced = await trace(_[0], opts);
-  console.log(traced.map(d => d.path).join("\n"));
+  console.log(traced.map(d => d.resolvedPath).join("\n"));
 })();
