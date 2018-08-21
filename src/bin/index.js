0688137d62d5c8c508ff063cc5ffba10129b1236
Trey Shugart <treshugart@gmail.com>
2018-08-21 12:17:30 +1000

    Fix bin.

--- src/bin/index.js
+++ src/bin/index.js
@@ -1,11 +1,8 @@
-#!/usr/bin/env node
-
 const { trace } = require("..");
 const yargs = require("yargs");
 const { _, ...opts } = yargs.argv;
 
-console.log(
-  trace(_[0], opts)
-    .map(d => d.path)
-    .join("\n")
-);
+(async () => {
+  const traced = await trace(_[0], opts);
+  console.log(traced.map(d => d.path).join("\n"));
+})();
